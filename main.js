85be57feb304dc91b97a992588b18043e53e392e
Connor Ashlock <connor9804@yahoo.com>
2020-07-12 21:29:23 -0700

    Completed meme-gallery

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-var imageUrls = [];
+var gallery = document.querySelector('main')
+
+var imageUrls = ['https://pbs.twimg.com/media/EctY-jKU4AAyc4f?format=jpg&name=900x900',
+                 'https://img-9gag-fun.9cache.com/photo/aBm2rNQ_700bwp.webp',
+                 'https://img-9gag-fun.9cache.com/photo/aAebRVE_700bwp.webp'
+                  ];
 
 var imageUrlInput = document.querySelector('input')
 var addImageButton = document.querySelector('button')
@@ -9,4 +14,17 @@ addImageButton.addEventListener('click',function(){
     imageUrls.push(imageUrlInput.value);
   }
   imageUrlInput.value = ''
+  updateGallery()
 })
+
+function updateGallery() {
+  gallery.innerHTML = ''
+  for (var i = 0; i < imageUrls.length; i++) {
+    var imageElement = document.createElement('img');
+    imageElement.classList.add('gallery-image');
+    imageElement.src = imageUrls[i];
+    gallery.appendChild(imageElement);
+  }
+}
+
+updateGallery();
